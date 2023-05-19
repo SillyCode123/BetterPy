dd36fc07011e60de545531fdede1cdfe35048929
Kindskopf123 <gehristeffi@gmail.com>
2023-05-19 15:08:15 +0200

    Made very good comments for each file
    gimme all your money

--- src/Formatter.py
+++ src/Formatter.py
@@ -1,29 +1,37 @@
 def format(build):
+    # split the build string into lines
     buildLines = build.splitlines()
     
-    tabs =0
-    curTabs = ""
-    formated = ""
+    tabs =0    # keep track of current indent level
+    curTabs = ""    # store current indentation spaces
+    formated = ""   # string to store formated code
+    
+    # go through each line in the build string
     for curS in buildLines:
-        curTabs = ""
+        curTabs = "" # reset intendation level
         
+        # add the right number of indentation spaces
         for _ in range(0, tabs):
             curTabs += "    "
             
-        cur = ""
+        cur = ""    # stores modified line
         
+        # check for open curly brackets
         if "{" in curS:
-            tabs += 1
-            cur = curS.replace("{", ":")
+            tabs += 1   # increase intendation level
+            cur = curS.replace("{", ":")    # replace "{" with ":"
         
+        # Check for closed curly brackets
         elif "}" in curS:
             tabs -= 1
-            cur = curS.replace("}", "")
+            cur = curS.replace("}", "") # replace "}" with nothing
             
+        # else set the line to the line
         elif curS:
             cur = curS 
     
         if curS:
+            # Add the formatted line with the right indentation to get the final result
             formated += curTabs + cur.strip() + "\n"    
  
     return formated.strip()
